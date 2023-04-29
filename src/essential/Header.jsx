6eaf8b0feec270a8f856fb9a7f992157a686422f
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-04-30 01:00:34 +0200

    first final header

--- src/essential/Header.jsx
+++ src/essential/Header.jsx
@@ -4,14 +4,21 @@ import "../css/Header.css"
 export default function Header(){
 
     return(
-        <div>
+        <div className="nav">
+
+            <div className="logo">
+                <img src="https://www.alternance-professionnelle.fr/wp-content/uploads/2020/09/LOGO-AFPA-VERT-PNG.png" alt="logo afpa" />
+            </div>
+
            <nav>
-                <ul>
-                    <li>
-                        mager
-                    </li>
-                </ul>
+                <a className="link" href="/">Accueil</a>
+                <a className="link" href="/">Nos formations</a>
+                <a className="link" href="/">Actualités</a>
+                <a className="link" href="/">Groupe Afpa</a>
+                <a className="login" href="/">login</a>
+                <a className="signUp" href="/">Sign up</a>
             </nav> 
+
         </div>
     )
 }
