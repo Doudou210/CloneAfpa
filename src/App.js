4db61e9bfc6b6f97e16c6be17c75b841e4648ada
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-04-29 23:05:48 +0200

    modification of header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Routes, Route} from 'react-router-dom';
 import Home from './page/Home';
+import Header from './essential/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Home/>
       <Routes>
         <Route path='/' component={Home}/>
---
+++ src/essential/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/Header.css"
+
+export default function Header(){
+
+    return(
+        <div>
+           <nav>
+                <ul>
+                    <li>
+                        mager
+                    </li>
+                </ul>
+            </nav> 
+        </div>
+    )
+}
