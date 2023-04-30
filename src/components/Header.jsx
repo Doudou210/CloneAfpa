3641971fd78968b0431c63e6291435ac67934762
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-05-01 01:26:29 +0200

    login commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,6 +15,43 @@ export default function Header(){
                     <button className="btnLogin" type="button" onClick={'Login'}>Login</button>
                 </nav> 
             </header>
+
+            <div className="wrapper">
+                <div className="form-box login">
+                    <h2>Login</h2>
+                    <span><ion-icon name="close-outline"></ion-icon></span>
+
+                    <form action="#">
+                        <div className="input_box">
+                            <span className="icon">
+                            <ion-icon name="mail"></ion-icon>
+                            </span>
+                            <input type="email" required/>
+                            <label type="Email">Email</label>
+                        </div>
+
+                        <div className="input_box">
+                            <span className="icon">
+                            <ion-icon name="lock-closed"></ion-icon>
+                            </span>
+                            <input type="password" required/>
+                            <label htmlFor="Password">Password</label>
+                        </div>
+
+                        <div className="remember-forgot">
+                            <label><input type="checkbox"/>Remember me</label>
+                            <a href="/">Forgot Password ?</a>
+                        </div>
+
+                        <button className="btn" type="submit">Login</button>
+
+                        <div className="login__register">
+                            <p>Don't have an account ? <a href="/" className="register__link">Register</a></p>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
