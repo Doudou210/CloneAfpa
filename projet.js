7fa17183a465ae1cb8685b3658d90682d0f397ab
Doudou210 <siakakoudou.doumbia@yahoo.com>
2023-07-10 01:49:34 +0200

    update and add selction move

--- projet.js
+++ projet.js
@@ -6,20 +6,33 @@ $materialSO.addEventListener('click', function(){
     $search.classList.toggle('active');
 })
 
-let $body = document.querySelector('body');
-
 window.onscroll= () =>{
     let $head = document.querySelector('header');
     let scroll = window.pageYOffset;
 
     if (scroll < 10) {
-        $head.style.background = '#D9D9D9';
-        $body.style.background = 'novajowhite';
+        $head.style.background = '#d9d9d9';
         $materialSO.style.color='black'
     }
     else {
         // transparent
-        $head.style.background = '#D9D9D9';
-        // $head.style.background = 'transparent';
+        // $head.style.background = 'red';
+        $head.style.background = 'transparent';
     } 
+}
+
+//Pour selection dans header
+let $navBar = document.querySelectorAll('header nav ul li');
+
+//Un boucle for pour selection
+for (let i = 0; i < $navBar.length; i++) {
+    $navBar[i].addEventListener('click', ()=>{
+        let $activeBtn = document.querySelector('header nav ul li.actived');
+
+        console.log('marche');
+        //ajouter ou retirer classes actives.
+        $activeBtn.classList.toggle('actived');
+        $navBar[i].classList.toggle('actived');
+    })
+    
 }
